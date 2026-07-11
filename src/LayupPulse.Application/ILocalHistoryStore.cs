96021ef579af749c8ae174729780568b7c2c6831
Arnaud Wissart <arnaud.wissart@live.fr>
2026-07-11 05:21:17 +0200

    refactor(application): retire les ports de persistance inutilisés

--- src/LayupPulse.Application/ILocalHistoryStore.cs
+++
@@ -1,9 +0,0 @@
-namespace LayupPulse.Application;
-
-/// <summary>
-/// Efface de manière atomique les données de démonstration persistées localement.
-/// </summary>
-public interface ILocalHistoryStore
-{
-    public Task ClearAsync(CancellationToken cancellationToken);
-}
